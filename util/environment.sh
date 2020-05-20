fd4926ce02c9d551271d53bc07cd21e5d35179de
Philip <caplan.philip@gmail.com>
2020-05-20 09:47:23 -0400

    bad path again

--- util/environment.sh
+++ util/environment.sh
@@ -24,8 +24,8 @@ if [[ `hostname` == *"wazowski"* ]]; then
   export LD_LIBRARY_PATH=/usr/lib:$LD_LIBRARY_PATH
 
   #export LAPACK_DIR=/home/gitlab-runner/lapack/lapack-3.6.1/install
-  export ESP_DIR=/home/gitlab-runner/EngSketchPad
-  export CAS_DIR=/home/gitlab-runner/OpenCASCADE-7.3.1
+  export ESP_DIR=/home/gitlab-runner/Codes/EngSketchPad
+  export CAS_DIR=/home/gitlab-runner/Codes/OpenCASCADE-7.3.1
   #export PARMETIS_DIR=/home/gitlab-runner/parmetis/parmetis-4.0.3/install
   #export METIS_DIR=/home/gitlab-runner/parmetis/metis-5.1.0/install
   #export PATH=/home/gitlab-runner/lcov/lcov-1.13/bin:$PATH
