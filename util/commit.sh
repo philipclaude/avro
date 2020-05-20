cd46f1549ed71d37fd6d4189683be11d50d62fa6
Philip <caplan.philip@gmail.com>
2020-05-20 09:19:16 -0400

    added util scripts for ci

---
+++ util/commit.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+workspace = `pwd`
+config=$1
+
+
+#Files might linger if a build was aborted
+echo "Removing any lingering untracked files"
+for f in `git ls-files --others --exclude-standard`; do
+  rm -f $f
+done
+
+#Create the build directory if it does not exist
+cmakedir=build/$config
+mkdir -p $cmakedir
+cd $cmakedir
+
+
+if [[ $config == *"coverage"* ]]; then
+  CMAKE_ARGS=""
+else
+  CMAKE_ARGS=""
+fi
+if [[ $config == *"memcheck"* ]]; then
+  CMAKE_ARGS="$CMAKE_ARGS -DUSE_MPI=OFF"
+fi
+
+# there is a bug in gcc 4.8 which prevents from using some features needed by mpi wrapper
+if [[ $config == *"gnu48"* ]]; then
+  CMAKE_ARGS="$CMAKE_ARGS -DUSE_MPI=OFF"
+fi
+
+time source $workspace/util/jenkins/configure.sh $CMAKE_ARGS
+
+# number of processors used in compile
+nproc=1
+
+if [[ $config == *"coverage"* ]]; then
+  # just because we have to clean, use more processors for coverage
+  nproc=6
+
+  # coverage information always needs to be completely recompiled
+  time make coverage_cleaner
+fi
+
+# build all libraries and executables
+time make -j $nproc
+
+if [[ $config == *"coverage"* ]]; then
+
+  time make unit_coverage
+
+  time make coverage_info
+  if [[ `hostname` == "wazowski" ]]; then
+    #time python $HOME/util/lcov/lcov_cobertura.py coverage.info -o $WORKSPACE/build/coverage.xml
+  fi
+  # generate html documents
+  #time make genhtml
+  # udpdate coverage information on acdl (@marshall do i need to rename this?)
+  #time rsync -rtuc --delete avroCoverageHTML jenkins@acdl.mit.edu:/var/lib/jenkins/userContent/
+
+fi
+
+# warning if any files were generated.
+# count the number of files
+cd $workspace
+tmpfiles=`git ls-files --others --exclude=build --exclude=app/WebViewer | wc -l`
+
+if [ $tmpfiles -ne 0 ]; then
+  echo "warning: Files should not be generated in code pushed to the repository."
+  echo "warning: Files found :"
+  for f in `git ls-files --others --exclude=build --exclude=external`; do
+    echo "warning: $f"
+  done
+fi
---
+++ util/environment.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+shopt -s nocasematch
+
+dir=`pwd`
+builddir=`basename $dir`
+
+if [[ `hostname` == *"wazowski"* ]]; then
+
+  if [[ $builddir == *"intel14"* ]]; then
+    source /opt/intel/composer_xe_2013_sp1/bin/compilervars.sh intel64 > iccvars.out 2>&1
+  elif [[ $builddir == *"intel15"* ]]; then
+    source /opt/intel/composer_xe_2015/bin/compilervars.sh intel64 > iccvars.out 2>&1
+  elif [[ $builddir == *"intel17"* ]]; then
+    source /opt/intel/compilers_and_libraries_2017/linux/bin/compilervars.sh intel64 > iccvars.out 2>&1
+  elif [[ $builddir == *"intel18"* ]]; then
+    source /opt/intel/compilers_and_libraries_2018/linux/bin/compilervars.sh intel64 > iccvars.out 2>&1
+  #else
+  #  source /opt/intel/bin/iccvars.sh intel64 > iccvars.out 2>&1
+  fi
+
+  # this makes sure cmake finds open-mpi rather than intel mpi
+  export PATH=/usr/bin:$PATH
+  export LD_LIBRARY_PATH=/usr/lib:$LD_LIBRARY_PATH
+
+  export LAPACK_DIR=/home/gitlab-runner/lapack/lapack-3.6.1/install
+  export ESP_DIR=/home/gitlab-runner/ESP/EngSketchPad
+  export CAS_DIR=/home/gitlab-runner/ESP/OpenCASCADE-7.4.1
+  #export PARMETIS_DIR=/home/gitlab-runner/parmetis/parmetis-4.0.3/install
+  #export METIS_DIR=/home/gitlab-runner/parmetis/metis-5.1.0/install
+  #export PATH=/home/gitlab-runner/lcov/lcov-1.13/bin:$PATH
+
+else
+  if [ -z "$LAPACK_DIR" ]; then
+    echo "Please set LAPACK_DIR in your environment."
+    exit 0
+  fi
+fi
+
+env > env.log 2>&1
