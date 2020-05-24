831b581ceefe55e1d5b2c0d2afc8aa8b3de09a71
Philip <caplan.philip@gmail.com>
2020-05-23 20:23:09 -0400

    regression testing

--- test/regression/benchmark3d.sh
+++ test/regression/benchmark3d.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export avro=/Users/pcaplan/Codes/gitlab/mach/avro/build/release/bin/avro
+export avro=$1
 
-$avro -adapt CKF-3-3-3 box Linear-3d cl rho=false write_conformity=true > cl.txt &
-$avro -adapt cube-cylinder.mesh cube-cylinder.egads Linear-3d ccl rho=false write_conformity=true > ccl.txt &
-$avro -adapt cube-cylinder.mesh cube-cylinder.egads Polar1 ccp1 rho=false write_conformity=true > ccp1.txt &
-$avro -adapt cube-cylinder.mesh cube-cylinder.egads Polar2 ccp2 rho=false write_conformity=true > ccp2.txt &
+$avro -adapt CKF-3-3-3 box Linear-3d $3/cl rho=false write_conformity=true > cl.txt &
+$avro -adapt $2/meshes/cube-cylinder.mesh $2/geometry/cube-cylinder.egads Linear-3d $3/ccl rho=false write_conformity=true > ccl.txt &
+$avro -adapt $2/meshes/cube-cylinder.mesh $2/geometry/cube-cylinder.egads Polar1 $3/ccp1 rho=false write_conformity=true > ccp1.txt &
+$avro -adapt $2/meshes/cube-cylinder.mesh $2/geometry/cube-cylinder.egads Polar2 $3/ccp2 rho=false write_conformity=true > ccp2.txt &
