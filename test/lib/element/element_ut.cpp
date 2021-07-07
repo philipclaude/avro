83dca57f429fbf65866adc4df26bc2421adcdcd0
pcaplan <pcaplan@mit.edu>
2021-07-06 22:18:59 -0400

    unit test fixes

--- test/lib/element/element_ut.cpp
+++ test/lib/element/element_ut.cpp
@@ -19,15 +19,7 @@ UT_TEST_SUITE( element_test_suite )
 UT_TEST_CASE( simplex_element_tests )
 {
   Simplex element( 2 , 1 );
-
-  ConicalProductQuadrature quadrature( -1 );
-  element.load_quadrature( quadrature );
-
   element.set_basis( BasisFunctionCategory_Lagrange );
-
-  real_t x[2] = {0.5,0.5};
-  real_t phi[3];
-  element.eval(x,phi);
 
 }
 UT_TEST_CASE_END( simplex_element_tests )
