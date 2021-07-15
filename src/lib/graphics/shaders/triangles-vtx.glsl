e166ab0fa322fea5a3ece2b677195ff196d83eb1
pcaplan <pcaplan@mit.edu>
2021-07-15 17:47:32 -0400

    added some macros to to shaders in library, because gl did not like that some varyings were not set

--- src/lib/graphics/shaders/triangles-vtx.glsl
+++ src/lib/graphics/shaders/triangles-vtx.glsl
@@ -5,8 +5,18 @@ uniform mat4 u_ModelViewProjectionMatrix;
 
 out vec3 v_Position;
 
+#if WITH_TESSELLATION == 0
+out vec3 v_Normal;
+out vec2 v_ParameterTess;
+#endif
+
 void main()
 {
   v_Position  = a_Position; // pass the position to the geometry shader
   gl_Position = vec4(a_Position,1.0);
+
+  #if WITH_TESSELLATION == 0
+  v_Normal = vec3(0,0,1);
+  v_ParameterTess = vec2(0,0);
+  #endif
 }
